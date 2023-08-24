dd09d811fe3a822f242254fabd2131cd88db9d3a
888Greys <toxicgreys001@gmail.com>
2023-08-24 15:22:58 -0700

    Mandatory task

---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+*f_nop- nothing in the stack
+*@head: header stack
+*@counter: line_number
+*Return: no return
+*/
+void f_nop(stack_t **head, unsigned int counter)
+{
+(void) counter;
+(void) head;
+}
